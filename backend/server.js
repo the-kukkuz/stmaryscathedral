a64f691db66ac75f5485c130d36c0ecb255f3886
futuretechie4u-collab <futuretechie4u@gmail.com>
2026-04-12 19:54:05 +0530

    raiwlay deploy

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,11 @@ app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
-    scriptSrc: ["'self'"],
-    styleSrc: ["'self'", "'unsafe-inline'"],
-    imgSrc: ["'self'", "data:"]
+    connectSrc: ["'self'", "*"], // Allow 'self' for API and * for troubleshooting (can tighten later)
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+    fontSrc: ["'self'", "https://fonts.gstatic.com"],
+    imgSrc: ["'self'", "data:", "blob:"]
   }
 }));
 
